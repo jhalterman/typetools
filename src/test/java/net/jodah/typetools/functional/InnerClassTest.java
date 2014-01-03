9f8981f58b1ebec66e473bbe13aaa7e08a0a85de
Jonathan Halterman <jhalterman@gmail.com>
2014-01-02 21:40:01 -0800

    Added support for resolving type arguments on inner classes.

---
+++ src/test/java/net/jodah/typetools/functional/InnerClassTest.java
@@ -0,0 +1,34 @@
+package net.jodah.typetools.functional;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.jodah.typetools.TypeResolver;
+import net.jodah.typetools.functional.InnerClassTest.FooPrime.BarPrime;
+
+import org.testng.annotations.Test;
+
+/**
+ * Tests that type arguments on inner classes are supported.
+ */
+@Test
+public class InnerClassTest {
+  public static class Foo<T extends Number> {
+    @SuppressWarnings("serial")
+    public class Bar extends ArrayList<T> {
+    }
+  }
+
+  public static class FooPrime extends Foo<Integer> {
+    @SuppressWarnings("serial")
+    public class BarPrime extends Bar {
+    }
+  }
+
+  @Test
+  public void shouldResolveTypeArgumentOnInnerClass() {
+    assertEquals(TypeResolver.resolveRawArgument(List.class, BarPrime.class), Integer.class);
+  }
+}
